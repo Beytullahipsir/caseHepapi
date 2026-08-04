60d428feea13d687f8e0c91afb5cad35a0797e74
bahar ipşir <bahar@192.168.1.38>
2026-08-04 16:14:34 +0300

    docker file oluşturuldu, compose.yaml yazıldı, şifre ve kullanıcı adı compose'a gömüldü şuanlık, mongo bunu alabiliyor sorun yok,

--- src/config.py
+++ src/config.py
@@ -1,3 +1,7 @@
 import json
+import os
 
-config = json.load(open('db_config.json'))  #load db in json format
+config = json.load(open('db_config.json'))  # load db in json format
+
+config["db"]["url"] = os.getenv("MONGO_URL", config["db"]["url"])
+config["db"]["name"] = os.getenv("MONGO_DB", config["db"]["name"])
