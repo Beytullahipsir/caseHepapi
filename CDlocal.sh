9790e6750417a9c4d5552eca72c16f18e74979b3
bahar ipşir <bahar@192.168.1.47>
2026-08-19 00:42:37 +0300

    Local CD denemeleri

---
+++ CDlocal.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO=$(yq '.flask-mongo.image.repository' ./webapp/values.yaml)
+TAG=$(yq '.flask-mongo.image.tag' ./webapp/values.yaml)
+RELEASE_NAME=$(yq '.releaseName' ./webapp/values.yaml)
+
+docker build -t "$REPO:$TAG" .
+minikube image load "$REPO:$TAG"
+helm upgrade --install "$RELEASE_NAME" ./webapp 
+
+kubectl get pods
+kubectl get ingress
